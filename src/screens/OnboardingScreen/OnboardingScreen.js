e40bb6a14f080312912b7b875c280814470a269d
Justin Chang <justin.p.chang@gmail.com>
2021-08-03 11:04:25 -0400

    Remove debug message

--- src/screens/OnboardingScreen/OnboardingScreen.js
+++ src/screens/OnboardingScreen/OnboardingScreen.js
@@ -25,7 +25,6 @@ export default function OnboardingScreen({ navigation }) {
 	}, []);
 
 	React.useEffect(() => {
-		console.log(metrics);
 		setHeight(metrics.height);
 		setWeight(metrics.weight);
 		setAge(metrics.age);
