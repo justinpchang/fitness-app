d7a82d26e047a597c6dd1d535c0c12097431af42
Justin Chang <justin.p.chang@gmail.com>
2021-08-03 08:49:28 -0400

    Move signIn to auth store

--- src/screens/LoginScreen/LoginScreen.js
+++ src/screens/LoginScreen/LoginScreen.js
@@ -3,40 +3,21 @@ import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import styles from './styles';
 import { firebase } from '../../firebase/config';
+import useAuthStore from '../../stores/AuthStore.js';
 
 export default function LoginScreen({ navigation }) {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const signIn = useAuthStore(state => state.signIn);
 
     const onFooterLinkPress = () => {
         navigation.navigate('Registration');
     };
 
     const onLoginPress = () => {
-        firebase
-            .auth()
-            .signInWithEmailAndPassword(email, password)
-            .then((response) => {
-                const uid = response.user.uid;
-                const usersRef = firebase.firestore().collection('users');
-                usersRef
-                    .doc(uid)
-                    .get()
-                    .then((firestoreDocument) => {
-                        if (!firestoreDocument.exists) {
-                            alert('User does not exist anymore.');
-                            return;
-                        }
-                        const user = firestoreDocument.data();
-                        navigation.navigate('Home', { user: user });
-                    })
-                    .catch((error) => {
-                        alert(error);
-                    });
-            })
-            .catch((error) => {
-                alert(error);
-            });
+        signIn(email, password).then((user) => {
+            navigation.navigate('Home', { user });
+        });
     };
 
     return (
--- src/stores/AuthStore.js
+++ src/stores/AuthStore.js
@@ -4,6 +4,20 @@ import { firebase } from '../firebase';
 
 const useAuthStore = create((set) => ({
 	user: null,
+	signIn: (email, password) => {
+		return firebase.auth().signInWithEmailAndPassword(email, password).then((res) => {
+			const uid = res.user.uid;
+			return firebase.firestore().collection('users').doc(uid).get();
+		}).then((doc) => {
+			if (!doc.exists) {
+				alert('User does not exist.');
+				return;
+			}
+			return doc.data();
+		}).catch((err) => {
+			alert(error);
+		});
+	},
 	signOut: () => {
 		set({ user: null });
 		return firebase.auth().signOut();
